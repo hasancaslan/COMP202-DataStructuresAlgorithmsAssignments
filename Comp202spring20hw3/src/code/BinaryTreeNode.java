e875771930cd474a5dad29c510ff2c11d9b9b517
Hasan Can <aslan4044@gmail.com>
2020-04-08 04:06:39 +0300

    HW3 - BinaryTreeNode implemented

--- Comp202spring20hw3/src/code/BinaryTreeNode.java
+++ Comp202spring20hw3/src/code/BinaryTreeNode.java
@@ -5,24 +5,45 @@ import given.Entry;
 /*
  * The binary node class which extends the entry class.
  * This will be used in linked tree implementations
- * 
+ *
  */
 public class BinaryTreeNode<Key, Value> extends Entry<Key, Value> {
-  
-  /*
-   * 
-   * YOUR CODE HERE
-   * 
-   */
-  public BinaryTreeNode(Key k, Value v) {
-    super(k, v);
-    
-    /*
-     * 
-     * This constructor is needed for the autograder. You can fill the rest to your liking.
-     * YOUR CODE AFTER THIS:
-     * 
-     */
-  }
+    private BinaryTreeNode<Key, Value> parent;
+    private BinaryTreeNode<Key, Value> left;
+    private BinaryTreeNode<Key, Value> right;
 
+    public BinaryTreeNode(Key k, Value v) {
+        super(k, v);
+    }
+
+    public BinaryTreeNode(Key k, Value v, BinaryTreeNode<Key, Value> parent, BinaryTreeNode<Key, Value> leftChild, BinaryTreeNode<Key, Value> rightChild) {
+        this(k, v);
+        this.parent = parent;
+        left = leftChild;
+        right = rightChild;
+    }
+
+    public code.BinaryTreeNode<Key, Value> getLeft() {
+        return left;
+    }
+
+    public void setLeft(code.BinaryTreeNode<Key, Value> left) {
+        this.left = left;
+    }
+
+    public code.BinaryTreeNode<Key, Value> getRight() {
+        return right;
+    }
+
+    public void setRight(code.BinaryTreeNode<Key, Value> right) {
+        this.right = right;
+    }
+
+    public code.BinaryTreeNode<Key, Value> getParent() {
+        return parent;
+    }
+
+    public void setParent(code.BinaryTreeNode<Key, Value> parent) {
+        this.parent = parent;
+    }
 }
