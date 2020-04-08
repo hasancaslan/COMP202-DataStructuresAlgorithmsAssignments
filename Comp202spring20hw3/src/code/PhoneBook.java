b085914e0e584ff694ca9e3b996275dd4c705db5
Hasan Can <aslan4044@gmail.com>
2020-04-08 04:07:01 +0300

    HW3 - PhoneBook implemented

--- Comp202spring20hw3/src/code/PhoneBook.java
+++ Comp202spring20hw3/src/code/PhoneBook.java
@@ -1,183 +1,157 @@
 package code;
 
-import java.util.List;
-
 import given.ContactInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * A phonebook class that should:
  * - Be efficiently (O(log n)) searchable by contact name
  * - Be efficiently (O(log n)) searchable by contact number
- * - Be searchable by e-mail (can be O(n)) 
- * 
+ * - Be searchable by e-mail (can be O(n))
+ *
  * The phonebook assumes that names and numbers will be unique
  * Extra exercise (not to be submitted): Think about how to remove this assumption
- * 
+ *
  * You need to use your own data structures to implement this
- * 
- * Hint: You need to implement a multi-map! 
- * 
+ *
+ * Hint: You need to implement a multi-map!
+ *
  */
 public class PhoneBook {
 
-  /*
-   * ADD MORE FIELDS IF NEEDED
-   * 
-   */
+    BinarySearchTree<String, ContactInfo> contactName;
+    BinarySearchTree<String, ContactInfo> contactNumber;
 
-  public PhoneBook() {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-  }
+    public PhoneBook() {
+        contactName = new BinarySearchTree<>();
+        contactNumber = new BinarySearchTree<>();
+        contactName.setComparator(String::compareTo);
+        contactNumber.setComparator(String::compareTo);
+    }
 
-  // Returns the number of contacts in the phone book
-  public int size() {
-    return 0;
-  }
+    // Returns the number of contacts in the phone book
+    public int size() {
+        return contactName.size();
+    }
 
-  // Returns true if the phone book is empty, false otherwise
-  public boolean isEmpty() {
-    return false;
-  }
+    // Returns true if the phone book is empty, false otherwise
+    public boolean isEmpty() {
+        return contactName.isEmpty();
+    }
 
-  //Adds a new contact or overwrites an existing contact with the given info
-  // Args should be given in the order of e-mail and address which is handled for
-  // you
-  // Note that it can also be empty. If you do not want to update a field pass
-  // null
-  public void addContact(String name, String number, String... args) {
-    int numArgs = args.length;
-    String email = null;
-    String address = null;
+    //Adds a new contact or overwrites an existing contact with the given info
+    // Args should be given in the order of e-mail and address which is handled for
+    // you
+    // Note that it can also be empty. If you do not want to update a field pass
+    // null
+    public void addContact(String name, String number, String... args) {
+        int numArgs = args.length;
+        String email = null;
+        String address = null;
 
-    /*
-     * Add stuff here if needed
-     */
+        if (numArgs > 0)
+            if (args[0] != null)
+                email = args[0];
+        if (numArgs > 1)
+            if (args[1] != null)
+                address = args[1];
+        if (numArgs > 2)
+            System.out.println("Ignoring extra arguments");
 
-    if (numArgs > 0)
-      if (args[0] != null)
-        email = args[0];
-    if (numArgs > 1)
-      if (args[1] != null)
-        address = args[1];
-    if (numArgs > 2)
-      System.out.println("Ignoring extra arguments");
+        ContactInfo contact = new ContactInfo(name, number);
+        contact.setAddress(address);
+        contact.setEmail(email);
+        contactName.put(name, contact);
+        contactNumber.put(number, contact);
+    }
 
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-  }
+    // Return the contact info with the given name
+    // Return null if it does not exists
+    // Should be O(log n)!
+    public ContactInfo searchByName(String name) {
+        return contactName.get(name);
+    }
 
-  // Return the contact info with the given name
-  // Return null if it does not exists
-  // Should be O(log n)!
-  public ContactInfo searchByName(String name) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return null;
-  }
+    // Return the contact info with the given phone number
+    // Return null if it does not exists
+    // Should be O(log n)!
+    public ContactInfo searchByPhone(String phoneNumber) {
+        return contactNumber.get(phoneNumber);
+    }
 
-  // Return the contact info with the given phone number
-  // Return null if it does not exists
-  // Should be O(log n)!
-  public ContactInfo searchByPhone(String phoneNumber) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return null;
+    // Return the contact info with the given e-mail
+    // Return null if it does not exists
+    // Can be O(n)
+    public ContactInfo searchByEmail(String email) {
+        for (BinaryTreeNode<String, ContactInfo> node : contactName.getNodesInOrder()) {
+            if (email.equals(node.getValue().getEmail()))
+                return node.getValue();
+        }
+        return null;
+    }
 
-  }
+    // Removes the contact with the given name
+    // Returns true if there is a contact with the given name to delete, false otherwise
+    public boolean removeByName(String name) {
+        ContactInfo contact = contactName.remove(name);
+        if (contact == null) return false;
+        contactNumber.remove(contact.getNumber());
+        return true;
+    }
 
-  // Return the contact info with the given e-mail
-  // Return null if it does not exists
-  // Can be O(n)
-  public ContactInfo searchByEmail(String email) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return null;
+    // Removes the contact with the given name
+    // Returns true if there is a contact with the given number to delete, false otherwise
+    public boolean removeByNumber(String phoneNumber) {
+        ContactInfo contact = contactNumber.remove(phoneNumber);
+        if (contact == null) return false;
+        contactName.remove(contact.getName());
+        return true;
+    }
 
-  }
+    // Returns the number associated with the name
+    public String getNumber(String name) {
+        ContactInfo contact = searchByName(name);
+        if (contact == null) return null;
+        return contact.getNumber();
+    }
 
-  // Removes the contact with the given name
-  // Returns true if there is a contact with the given name to delete, false otherwise
-  public boolean removeByName(String name) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return false;
-  }
+    // Returns the name associated with the number
+    public String getName(String number) {
+        ContactInfo contact = searchByPhone(number);
+        if (contact == null) return null;
+        return contact.getName();
+    }
 
-  // Removes the contact with the given name
-  // Returns true if there is a contact with the given number to delete, false otherwise
-  public boolean removeByNumber(String phoneNumber) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return false;
-  }
+    // Update the email of the contact with the given name
+    // Returns true if there is a contact with the given name to update, false otherwise
+    public boolean updateEmail(String name, String email) {
+        ContactInfo contact = searchByName(name);
+        if (contact == null) return false;
+        contact.setEmail(email);
+        return true;
+    }
 
-  // Returns the number associated with the name
-  public String getNumber(String name) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return null;
-  }
+    // Update the address of the contact with the given name
+    // Returns true if there is a contact with the given name to update, false otherwise
+    public boolean updateAddress(String name, String address) {
+        ContactInfo contact = searchByName(name);
+        if (contact == null) return false;
+        contact.setAddress(address);
+        return true;
+    }
 
-  // Returns the name associated with the number
-  public String getName(String number) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return null;
-  }
-  
-  // Update the email of the contact with the given name
-  // Returns true if there is a contact with the given name to update, false otherwise
-  public boolean updateEmail(String name, String email) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return false;
-  }
-  
-  // Update the address of the contact with the given name
-  // Returns true if there is a contact with the given name to update, false otherwise
-  public boolean updateAddress(String name, String address) {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return false;
-  }
+    // Returns a list containing the contacts in sorted order by name
+    public List<ContactInfo> getContacts() {
+        List<ContactInfo> contacts = new ArrayList<>();
+        for (BinaryTreeNode<String, ContactInfo> node : contactName.getNodesInOrder())
+            contacts.add(node.getValue());
+        return contacts;
+    }
 
-  // Returns a list containing the contacts in sorted order by name
-  public List<ContactInfo> getContacts() {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-    return null;
-  }
-
-  // Prints the contacts in sorted order by name
-  public void printContacts() {
-    /*
-     * TO BE IMPLEMENTED
-     * 
-     */
-  }
+    // Prints the contacts in sorted order by name
+    public void printContacts() {
+        System.out.println(getContacts());
+    }
 }
